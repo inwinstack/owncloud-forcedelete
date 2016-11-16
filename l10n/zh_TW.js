5ddc7db10e73cc96f19aff91c0ab603e2ed8db13
CurlyYang <eric.y@inwinstack.com>
2016-11-16 15:33:51 +0800

    add l10n translation

--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -2,5 +2,7 @@ OC.L10N.register(
     "forcedelete",
     {
     "Force Delete" : "強制刪除",
+    "File Delete" : "檔案刪除",
+    "Are you sure to force delete this file?" : "確定要強制刪除這些檔案？"
         },
 "nplurals=1; plural=0;");
